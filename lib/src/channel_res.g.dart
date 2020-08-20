8128825dea2828690c42649a71c26987a5a48b27
lathamios <nicholas@petbnb.nl>
2020-08-20 08:23:47 +0200

    Update channel_res.g.dart

--- lib/src/channel_res.g.dart
+++ lib/src/channel_res.g.dart
@@ -109,3 +109,43 @@ Map<String, dynamic> _$OpenChannelToJson(OpenChannel instance) {
   writeNotNull('custom_type', instance.customType);
   return val;
 }
+
+ChannelMetadata _$ChannelMetadataFromJson(Map json) {
+  return ChannelMetadata()
+    ..id = json['id'] as String
+    ..hostId = json['hostId'] as String
+    ..ownerId = json['ownerId'] as String
+    ..type = json['type'] as String
+    ..status = json['status'] as String
+    ..sitterFee = json['sitterFee'] as String
+    ..windowDisplay = json['windowDisplay'] as String
+    ..price = json['price'] as String
+    ..paymentDate = json['paymentDate'] as String
+    ..createDate = json['createDate'] as String
+}
+
+Map<String, dynamic> _$ChannelMetadataaToJson(Metadata instance) {
+  final val = <String, dynamic>{};
+
+  void writeNotNull(String key, dynamic value) {
+    if (value != null) {
+      val[key] = value;
+    }
+  }
+
+  writeNotNull('user_id', instance.userId);
+  writeNotNull('nickname', instance.nickname);
+  writeNotNull('profile_url', instance.profileUrl);
+  writeNotNull('id', instance.id);
+  writeNotNull('hostId', instance.hostId);
+  writeNotNull('ownerId', instance.ownerId);
+  writeNotNull('type', instance.type);
+  writeNotNull('status', instance.status);
+  writeNotNull('sitterFee', instance.sitterFee);
+  writeNotNull('windowDisplay', instance.windowDisplay);
+  writeNotNull('price', instance.price);
+  writeNotNull('paymentDate', instance.paymentDate);
+  writeNotNull('createDate', instance.createDate);
+  return val;
+}
+
