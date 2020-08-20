89a88b0358b784b2aaa9b546126fb81454e911fb
lathamios <nicholas@petbnb.nl>
2020-08-20 11:00:57 +0200

    Create meta_res.dart

---
+++ lib/src/meta_res.dart
@@ -0,0 +1,21 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'metadata_res.g.dart';
+
+@JsonSerializable(fieldRename: FieldRename.snake, includeIfNull: false, anyMap: true)
+class Metadata {
+  Metadata();
+  factory Metadata.fromJson(Map<String, dynamic> map) => _$MemberFromJson(map);
+  Map<String, dynamic> toJson() => _$MetaDataToJson(this);
+
+  String id;
+  String hostId;
+  String ownerId;
+  String type;
+  String status;
+  String sitterFee;
+  String windowDisplay;
+  String price;
+  String paymentDate;
+  String createDate;
+}
